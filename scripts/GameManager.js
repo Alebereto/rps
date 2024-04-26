10293c6fe506aad9f67419e522e864a323424325
Albert Khayat <1albertkhayat1@gmail.com>
2024-04-26 15:02:11 +0300

    Restructure game

--- scripts/GameManager.js
+++ scripts/GameManager.js
@@ -1,14 +1,187 @@
+import * as THREE from 'three';
 
-let instance = null;
+/**
+ * Class containing all elements of the world
+ */
+class Stage {
+    #canvas;
+    #scene;
+    #camera;
+    #renderer;
 
+    /**
+     * @param {*} canvas tag that game is rendered on
+     */
+    constructor(canvas) {
+        this.#canvas = canvas;
+
+        // scene
+        this.#scene = new THREE.Scene();
+        // camera
+        this.#camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        this.#camera.position.z = 5;
+        // renderer
+        this.#renderer = new THREE.WebGLRenderer({
+            canvas: this.#canvas,
+        });
+
+        this.onWindowResize();  // initial size
+        window.addEventListener('resize', () => this.onWindowResize());
+    }
+
+    get camera() {
+        return this.#camera;
+    }
+    get scene() {
+        return this.#scene;
+    }
+
+    onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+    
+        this.#renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+
+    render() {
+        this.#renderer.render( this.scene, this.camera );
+    }
+
+    add( obj ) {
+        this.scene.add(obj);
+    }
+
+    remove( obj ) {
+        this.scene.remove(obj);
+    }
+
+}
+
+/**
+ * Singleton class that manages the game
+ */
 class GameManager {
+    
+    static #instance = null;    // GameManager instance
+
+    // main html elements
+    #canvas = document.querySelector("#bg");    // game is rendered on canvas
+    #container = document.querySelector("#container");  // ui is on container
+
+    // game states
+    STATES = {LOADING: 0, SELECTING: 1};
+    #state = this.STATES.LOADING;
+
+    // world
+    #stage;
+    #raycaster;
+    #clock;
+    #deltaTime = 0;
+
+    // groups
+    #choices = [];
+
     constructor() {
-        if (instance === null) {
-            instance = this;
+        this.#stage = new Stage(this.canvas);
+
+        this.#raycaster = new THREE.Raycaster();
+        this.#clock = new THREE.Clock();
+    }
+
+    get stage() {
+        return this.#stage;
+    }
+    get choices() {
+        return this.#choices;
+    }
+    get canvas() {
+        return this.#canvas;
+    }
+    get container() {
+        return this.#container;
+    }
+
+    newGame() {
+        this.playerScore = 0;
+        this.oponentScore = 0;
+    }
+
+    castRay(x, y) {
+        const mouseX = (x / window.innerWidth) * 2 - 1;
+        const mouseY = - (y / window.innerHeight) * 2 + 1;
+    
+        this.#raycaster.setFromCamera({
+            x: mouseX,
+            y: mouseY
+        }, this.stage.camera);
+    
+        return this.#raycaster.intersectObjects(this.stage.scene.children);
+    }
+
+    /**
+     * Rotates choice
+     * @param {Choice} choice choice object
+     * @param {number} deltaTime time passed since last animate() call
+     */
+    rotate( choice, deltaTime ) {
+        const speed = 1.3;
+        choice.rotation.x += speed * deltaTime;
+        choice.rotation.y += speed * deltaTime;
+    }
+
+    updateScale( choice, deltaTime ) {
+        const speed = 3;
+        let scale = choice.scale.x;
+        if (choice.hovered) {
+            if (scale < 1.3) {
+                scale = Math.min(1.3, scale + speed * deltaTime);
+            }
         }
+        else if (scale > 1) {
+            scale = Math.max(1, scale - speed * deltaTime);
+        }
+        choice.scale.x = scale;
+        choice.scale.y = scale;
+        choice.scale.z = scale;
+    }
+
+    add(array) {
+        for( const obj of array ) {
+            this.stage.add(obj);
+            this.#choices.push(obj);
+        }
+    }
+
+    updateStats(deltaTime) {
+        // for each choice
+        for (const choice of this.#choices) {
+            this.rotate(choice, deltaTime);
+            this.updateScale(choice, deltaTime);
+        }
+    }
+
+    /**
+     * 
+     */
+    gameUpdate() {
+        requestAnimationFrame( () => this.gameUpdate() );
+        this.#deltaTime = this.#clock.getDelta();
+
+        this.updateStats(this.#deltaTime);
+        this.stage.render();
+    }
+
+    /**
+     * Get singleton instance of GameManager
+     * @returns {GameManager} existing singleton GameManager
+     */
+    static getInstance() {
+        if (GameManager.#instance === null) {
+            GameManager.#instance = new GameManager();
+            Object.freeze(GameManager.#instance);
+        }
+        return GameManager.#instance;
     }
 }
 
-let gameManagerInstance = Object.freeze(new GameManager());
-
-export default gameManagerInstance
+export default GameManager;
--- scripts/threedee.js
+++ scripts/threedee.js
@@ -1,21 +1,9 @@
 import * as THREE from 'three';
+import GameManager from './GameManager.js'
 
 const IMAGE_PATH = 'public'
 
-
-// setup scene
-const canvas = document.querySelector("#bg");
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-});
-renderer.setSize( window.innerWidth, window.innerHeight );
-
-
-const raycaster = new THREE.Raycaster();
-const clock = new THREE.Clock();
+const gm = GameManager.getInstance();
 
 class Choice extends THREE.Mesh {
     constructor(name, texturePath) {
@@ -32,44 +20,8 @@ class Choice extends THREE.Mesh {
     }
 }
 
-// make choices
-const rock = new Choice("rock", IMAGE_PATH + '/rock.png');
-const paper = new Choice("paper", IMAGE_PATH + '/paper.png');
-const scissors = new Choice("scissors", IMAGE_PATH + '/scissors.png');
-
-let choices = [rock, paper, scissors];
-
-scene.add( rock );
-scene.add( paper );
-scene.add( scissors );
-
-rock.position.x = -3;
-scissors.position.x = 3;
-
-camera.position.z = 5;
-
-function onWindowResize() {
-    
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-function castRay(x, y) {
-    const mouseX = (x / window.innerWidth) * 2 - 1;
-    const mouseY = - (y / window.innerHeight) * 2 + 1;
-
-    raycaster.setFromCamera({
-        x: mouseX,
-        y: mouseY
-    }, camera);
-
-    return raycaster.intersectObjects(scene.children);
-}
-
 function clicked(event) {
-    const intersects = castRay(event.clientX, event.clientY)
+    const intersects = gm.castRay(event.clientX, event.clientY)
     
     if (intersects.length > 0) {
         const clickedMesh = intersects[0].object;
@@ -79,64 +31,23 @@ function clicked(event) {
 }
 
 function mouseMove(event) {
-    const intersects = castRay(event.clientX, event.clientY);
-    choices.forEach((choice) => choice.hovered = false);
+    const intersects = gm.castRay(event.clientX, event.clientY);
+    gm.choices.forEach((choice) => choice.hovered = false);
     if (intersects.length > 0) 
         intersects[0].object.hovered = true
 }
 
-/**
- * Rotates choice
- * @param {Choice} choice choice object
- * @param {number} deltaTime time passed since last animate() call
- */
-function rotate( choice, deltaTime ) {
-    const speed = 1.3;
-    choice.rotation.x += speed * deltaTime;
-    choice.rotation.y += speed * deltaTime;
-}
+// make choices
+const rock = new Choice("rock", IMAGE_PATH + '/rock.png');
+const paper = new Choice("paper", IMAGE_PATH + '/paper.png');
+const scissors = new Choice("scissors", IMAGE_PATH + '/scissors.png');
 
+gm.add([rock, paper, scissors]);
 
-function updateScale( choice, deltaTime ) {
-    const speed = 3;
-    let scale = choice.scale.x;
-    if (choice.hovered) {
-        if (scale < 1.3) {
-            scale = Math.min(1.3, scale + speed * deltaTime);
-        }
-    }
-    else if (scale > 1) {
-        scale = Math.max(1, scale - speed * deltaTime);
-    }
-    choice.scale.x = scale;
-    choice.scale.y = scale;
-    choice.scale.z = scale;
-}
+rock.position.x = -3;
+scissors.position.x = 3;
 
-function updateStats( deltaTime ) {
-    // for each choice
-    for (const choice of choices) {
-        rotate(choice, deltaTime);
-        updateScale(choice, deltaTime);
-    }
-}
+gm.container.addEventListener("mousedown", clicked);
+gm.container.addEventListener("mousemove", mouseMove);
 
-function render() {
-    renderer.render( scene, camera );
-}
-
-window.addEventListener('resize', onWindowResize);
-canvas.addEventListener('mousedown', clicked);
-canvas.addEventListener("mousemove", mouseMove);
-
-function animate() {
-	requestAnimationFrame( animate );
-
-    let deltaTime = clock.getDelta();
-
-    updateStats(deltaTime);
-    render();
-
-}
-
-animate();
+gm.gameUpdate();
